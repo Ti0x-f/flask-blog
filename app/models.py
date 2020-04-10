9b39cd382b3c39bdd20030cea597a534bd823c7e
Antoine Facerias <antoine.facerias@gmail.com>
2020-04-10 18:31:17 -0400

    Structure of a login page for admin dashboard access. Creation of a User model for ORM with SQLAlchemy.

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, index=True, unique=True)
+    password_hash = db.Column(db.String)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 from flask import render_template, redirect, url_for
+from app.forms import LoginDashboardForm
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    form = LoginDashboardForm()
+    if form.validate_on_submit():
+        return redirect(url_for('dashboard'))
+    return render_template('login_dashboard.html', title='Dashboard Login', form=form)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', title='Dashboard')
