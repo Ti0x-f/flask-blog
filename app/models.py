8b40104d2967de444a3e260bf24c535c7303a587
Antoine Facerias <antoine.facerias@gmail.com>
2020-04-10 18:42:08 -0400

    Password setting/checking

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, index=True, unique=True)
     password_hash = db.Column(db.String)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
