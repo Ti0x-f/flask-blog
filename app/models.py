e5d9522f37f1b11c76a4b806b6cab3465ec2ee56
Antoine Facerias <antoine.facerias@gmail.com>
2020-04-10 18:54:08 -0400

    Set up login/logout functions for admin dashboard

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, index=True, unique=True)
     password_hash = db.Column(db.String)
@@ -11,3 +12,7 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(id)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
+from flask_login import current_user, login_required, login_user, logout_user
 from app.forms import LoginDashboardForm
+from app.models import User
 from app import app
 
 @app.route('/')
@@ -9,11 +11,24 @@ def index():
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginDashboardForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('admin'))
+        login_user(user)
         return redirect(url_for('dashboard'))
     return render_template('login_dashboard.html', title='Dashboard Login', form=form)
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return render_template('dashboard.html', title='Dashboard')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
