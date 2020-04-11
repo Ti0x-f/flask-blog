51f29c0151e3a6caf607cfe2361feea90ef85a9f
Antoine Facerias <antoine.facerias@gmail.com>
2020-04-11 14:36:45 -0400

    Structure of the dashboard/New template to get all posts published

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, url_for, flash
 from flask_login import current_user, login_required, login_user, logout_user
-from app.forms import LoginDashboardForm
-from app.models import User
-from app import app
+from app.forms import LoginDashboardForm, NewPostForm
+from app.models import User, Post
+from app import app, db
 
 @app.route('/')
 @app.route('/index')
@@ -23,12 +23,26 @@ def admin():
         return redirect(url_for('dashboard'))
     return render_template('login_dashboard.html', title='Dashboard Login', form=form)
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    return render_template('dashboard.html', title='Dashboard')
+    form = NewPostForm()
+    if form.validate_on_submit():
+        new_post = Post(title=form.title.data, description=form.description.data,
+            body=form.body.data)
+        db.session.add(new_post)
+        db.session.commit()
+        flask("New post has been posted")
+        return redirect(url_for('dashboard'))
+    return render_template('dashboard.html', title='Dashboard', form=form)
+
+@app.route('/all_posts')
+@login_required
+def all_posts():
+    return render_template('all_posts.html', title='All posts')
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
